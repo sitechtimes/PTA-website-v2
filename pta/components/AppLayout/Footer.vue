4a847b56f16c605627876c5b2c188eeb43f85936
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-10-21 02:55:01 -0400

    hhh

--- pta/components/AppLayout/Footer.vue
+++ pta/components/AppLayout/Footer.vue
@@ -1,17 +1,46 @@
 <template>
-    <div class="flex flex-col min-h-screen">
-      <div class="flex-grow">
-        <NuxtPage />
-      </div>
-      <footer>
-        <div id="firstSection">
-          <img src="/logo/pta-logo.B2o53BpU.png" alt="PTA Logo" />
+  <footer>
+    <div id="sections" class="flex flex-col sm:flex-row my-7 mx-4 xl:mx-14 p-4">
+      <div id="firstSection" class="flex-1 w-full lg:w-auto mx-4 mb-8 lg:mb-0">
+        <img src="/logo/pta-logo.B2o53BpU.png" alt="PTA Logo" class="w-4/12" />
+        <div id="links" class="my-5 text-md">
+          <NuxtLink to="/">Home</NuxtLink> |
+          <NuxtLink to="/Events">Events</NuxtLink> |
+          <NuxtLink to="/Contact Us">Contact Us</NuxtLink> |
+          <NuxtLink to="/Donate">Donate</NuxtLink>
         </div>
-      </footer>
+        <h2 class="text-xs">SITHS PTA @2024</h2>
+      </div>
+      <div id="secondSection" class="flex-1 w-full lg:w-auto mx-4 mb-8 lg:mb-0">
+        <div id="email" class="flex my-4">
+          <img src="/icons/envelope.svg" alt="Email" class="w-6">
+          <h2 class="ml-3">sitechpta@gmail.com</h2>
+        </div>
+        <div id="location" class="flex my-4 mb-8">
+          <img src="/icons/location.png" alt="Location" class="w-6 h-7">
+          <h2 class="ml-3">485 Clawson Street, Staten Island, NY 10306</h2>
+        </div>
+        <div id="social media" class="flex">
+          <a href="https://www.instagram.com/_sammytheseagull/">
+            <img src="/social media/instagram.svg" alt="Instagram" class="mr-8">
+          </a>
+          <a href="https://www.facebook.com/SITECHPTA/">
+            <img src="/social media/facebook.svg" alt="Facebook">
+          </a>
+        </div>
+      </div>
+      <div id="thirdSection" class="flex-1 w-full lg:w-auto mx-4">
+        <h2 class="font-bold">About Us</h2>
+        <p class="text-sm my-3 mb-8">
+          The mission of Staten Island Tech’s PTA is to support our school through advocacy, volunteerism, and fundraising. From our bake sales to our annual Spring Gala, we work diligently to raise money in order to financially support our school’s needs such as online and software services, facility upgrades, and social events/celebrations.
+        </p>
+        <NuxtLink to="/Donate" class="bg-dark-brown text-white p-3 text-sm rounded-lg">Donate Now!</NuxtLink>
+      </div>
     </div>
-  </template>
-<script setup>
+  </footer>
+</template>
 
+<script setup>
 </script>
 
 <style scoped>
