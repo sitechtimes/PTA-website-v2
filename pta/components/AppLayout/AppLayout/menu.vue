4a6fb8e81d93cc68cb65fd42360bbdfe2ddd798a
katherine <116115224+kaerulean@users.noreply.github.com>
2024-10-21 22:47:04 -0400

    menu appaer and dissappear works?
    css time

--- pta/components/AppLayout/AppLayout/menu.vue
+++ pta/components/AppLayout/AppLayout/menu.vue
@@ -1,10 +1,8 @@
 <template>
     <div>
     <div class="menu">
-        <input v-model="checked" @click="toggle" type="checkbox" id="active" />
-        <!-- <label for="active" class="menu-btn"><span></span></label>
-        <label for="active" class="close"></label> -->
-      <div v-if="show" class="wrapper">
+      <input v-model="show" @click="toggle" type="checkbox"/>
+      <div v-if="show">
         <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50">
             <img class="icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> home
         </NuxtLink>
@@ -30,11 +28,11 @@
 import { ref } from 'vue';
 
 const show = ref(false)
-const checked = ref(false)
 
 function toggle(){
     show.value != show.value;
-    checked.value != checked.value;
+
+    console.log(show.value)
 }
 </script>
 
