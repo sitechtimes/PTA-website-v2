d9881ec60eeeac37bd69c134658566b7c92a0db3
katherine <116115224+kaerulean@users.noreply.github.com>
2024-10-22 20:13:51 -0400

    mq
    - row the words
    - make the slide menu

--- pta/components/AppLayout/AppLayout/NavBar.vue
+++ pta/components/AppLayout/AppLayout/NavBar.vue
@@ -1,40 +1,45 @@
 <script setup>
 import Menu from './menu.vue';
+// import { ref } from 'vue';
+
+// const donateShow = ref(true);
 </script>
 
 <template>
     <header id="navbar" class="grid grid-cols-2">
         <div id="logoContainer">
             <NuxtLink to="/" @click="('page=index')">
-                <img class="ml-6 mt-4 scale-75" id="ptaLogo" src="/public/navbarIcons/pta-logo.B2o53BpU.png">
+                <img class="ml-2 mt-2 sm:mt-4 lg:ml-6 scale-75" id="ptaLogo" src="/public/navbarIcons/pta-logo.B2o53BpU.png">
             </NuxtLink>
         </div>
 
-        <div id="naV" class="grid grid-cols-5">
-            <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50">
-                <img class="size-8 mt-10 icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> home
-            </NuxtLink>
-            
+        <div id="naV" class="grid grid-cols-4"> 
+             <div>
+                <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50 max-[1100px]:invisible">
+                    <img class="size-8 mt-10 icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> home
+                </NuxtLink>
+             </div>
 
-            
-            <NuxtLink to="/events" class="hover:opacity-50">
+            <div>
+                <NuxtLink to="/events" class="hover:opacity-50 max-[1100px]:invisible">
                 <img class="size-8 mt-10 icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg"> events
             </NuxtLink>
+            </div>
             
+            <div>
+                <NuxtLink to="/contacts" class="hover:opacity-50 max-[1100px]:invisible">
+                    <img class="size-8 mt-10 icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg"> join us
+                </NuxtLink>
+            </div>
 
-            
-            <NuxtLink to="/contacts" class="hover:opacity-50">
-                <img class="size-8 mt-10 icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg"> join us
-            </NuxtLink>
-            
-
-          
-            <NuxtLink to="/donate" class="hover:opacity-50">
-                <img class="size-8 mt-10 icon" id="donate" src="/public/navbarIcons/creditCard.svg"> donate
-            </NuxtLink>
-
-            <Menu></Menu>
+            <div>
+                <NuxtLink to="/donate" class="hover:opacity-50 max-[1100px]:invisible">
+                    <img class="size-8 mt-10 icon" id="donate" src="/public/navbarIcons/creditCard.svg"> donate
+                </NuxtLink>
+                <Menu class="invisible max-[1100px]:visible"></Menu>
+            </div>
         </div>
+
     </header>
     
 </template>
--- pta/components/AppLayout/AppLayout/menu.vue
+++ pta/components/AppLayout/AppLayout/menu.vue
@@ -4,19 +4,19 @@
       <input v-model="show" @click="toggle" type="checkbox"/>
       <div v-if="show">
         <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50">
-            <img class="icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> home
+            <img class="size-12 icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> home
         </NuxtLink>
         
         <NuxtLink to="/events" class="hover:opacity-50">
-            <img class="icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg"> events
+            <img class="size-12 icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg"> events
         </NuxtLink>
 
         <NuxtLink to="/contacts" class="hover:opacity-50">
-            <img class="icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg"> join us
+            <img class="size-12 icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg"> join us
         </NuxtLink>
 
         <NuxtLink to="/donate" class="hover:opacity-50">
-            <img class="icon" id="donate" src="/public/navbarIcons/creditCard.svg"> donate
+            <img class="size-12 icon" id="donate" src="/public/navbarIcons/creditCard.svg"> donate
         </NuxtLink>
         
       </div>
@@ -25,6 +25,7 @@
 </template>
 
 <script setup>
+import gsap from 'gsap';
 import { ref } from 'vue';
 
 const show = ref(false)
@@ -34,6 +35,8 @@ function toggle(){
 
     console.log(show.value)
 }
+
+
 </script>
 
 <style scoped>
