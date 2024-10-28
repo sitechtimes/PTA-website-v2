a74841ddb5ffab23d8abcd7c80eb0dd7ab374999
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-10-28 10:29:46 -0400

    waaa

--- pta/pages/contact.vue
+++ pta/pages/contact.vue
@@ -1,10 +1,13 @@
 <template>
-    <div id="menu" class="text-center mt-9">
+    <div id="container" class="mt-9 text-center">
         <h1 class="text-5xl font-bold">CONTACTS</h1>
-        <div id="buttons" class="text-4xl mt-8 font-medium ">
-            <button class="mx-28 hover:opacity-50 focus:bg-white p-3 rounded-xl">Join Us</button>
-            <button class="mx-28 hover:opacity-50">Staff</button>
-            <button class="mx-28 hover:opacity-50">SLT</button>
+        <div id="buttons" class="text-3xl mt-8 font-semibold">
+            <button class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">Join Us</button>
+            <button class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">Staff</button>
+            <button class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">SLT</button>
+        </div>
+        <div id="components" class="bg-white rounded-xl">
+            
         </div>
     </div>
 </template>
