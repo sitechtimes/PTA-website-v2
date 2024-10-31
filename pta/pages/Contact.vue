24fc306a78796777119cbe92b68564d09e80eb4e
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-10-31 10:58:27 -0400

    a

--- pta/pages/Contact.vue
+++ pta/pages/Contact.vue
@@ -1,28 +1,29 @@
 <template>
-    <div id="container" class="mt-9 text-center">
+    <div id="container" class="mt-9 text-center flex flex-col justify-center">
         <h1 class="text-5xl font-bold ">CONTACTS</h1>
         <div id="buttons" class="text-3xl mt-8 font-semibold ">
-            <button id="join" @click="buttonClick($event)" class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl ">Join Us</button>
-            <button id="staff" @click="buttonClick($event)" class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">Staff</button>
-            <button id="slt" @click="buttonClick($event)" class="mx-28 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">SLT</button>
+            <button id="join" @click="buttonClick($event)" class="mx-24 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl ">Join Us</button>
+            <button id="staff" @click="buttonClick($event)" class="mx-24 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">Staff</button>
+            <button id="slt" @click="buttonClick($event)" class="mx-24 hover:opacity-50 focus:bg-white px-12 py-2 rounded-xl">SLT</button>
         </div>
-        <div v-if="buttonType==='staff'" class="bg-white rounded-xl w-6/12">
-            <p>Different staff</p>
-        </div>
-        <div v-if="buttonType==='slt'" class="bg-white rounded-xl w-6/12">
-            <p>The SLT</p>
-        </div>
-        <div v-if="buttonType==='join'" class="bg-white rounded-xl w-6/12">
+        <div class="flex justify-center my-6">
+        <div v-if="buttonType==='join'" class="bg-white rounded-3xl w-6/12 p-12">
             <p>Join us</p>
         </div>
+        <div v-if="buttonType==='staff'" class="bg-white rounded-3xl w-6/12 p-12">
+            <p>Different staff</p>
+        </div>
+        <div v-if="buttonType==='slt'" class="bg-white rounded-3xl w-6/12 p-12">
+            <p>The SLT</p>
+        </div>
     </div>
+</div>
 </template>
 
 <script setup>
-const buttonType = ref('');
+const buttonType = ref('join');
 function buttonClick(event) {
     buttonType.value = event.target.getAttribute("id");
-    console.log(buttonType.value);
 }
 </script>
 
