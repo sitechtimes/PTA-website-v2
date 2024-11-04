ff37581d6b0f3eeb3c6e8a07d79f357054931162
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-11-04 10:56:51 -0500

    a

--- pta/pages/contact.vue
+++ pta/pages/contact.vue
@@ -4,22 +4,22 @@
         <div id="buttons" class="text-3xl mt-8 font-semibold ">
             <button id="join" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'join'}" 
-                class="mx-20 hover:opacity-50 w-1/12 py-2 rounded-xl ">Join Us</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 py-2 rounded-xl xl:mx-20">Join Us</button>
             <button id="staff" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'staff'}" 
-                class="mx-20 hover:opacity-50 w-1/12 py-2 rounded-xl">Staff</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 py-2 rounded-xl xl:mx-20">Staff</button>
             <button id="slt" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'slt'}" 
-                class="mx-20 hover:opacity-50 w-1/12 py-2 rounded-xl">SLT</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 py-2 rounded-xl xl:mx-20">SLT</button>
         </div>
         <div class="flex justify-center my-6">
-            <div v-if="buttonType === 'join'" class="bg-white rounded-3xl w-6/12 p-12">
+            <div v-if="buttonType === 'join'" class="bg-white rounded-3xl w-10/12 p-10">
                 <p>Join us</p>
             </div>
-            <div v-if="buttonType === 'staff'" class="bg-white rounded-3xl w-6/12 p-12">
+            <div v-if="buttonType === 'staff'" class="bg-white rounded-3xl w-10/12 p-10">
                 <p>Different staff</p>
             </div>
-            <div v-if="buttonType === 'slt'" class="bg-white rounded-3xl w-6/12 p-12">
+            <div v-if="buttonType === 'slt'" class="bg-white rounded-3xl w-10/12 p-10">
                 <p>The SLT</p>
             </div>
         </div>
