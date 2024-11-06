4a5f198f53ff048218014466c2a1bb9001706e39
katherine <116115224+kaerulean@users.noreply.github.com>
2024-11-05 22:11:41 -0500

    slide
    - just hamburg slide

--- pta/components/AppLayout/AppLayout/NavBar.vue
+++ pta/components/AppLayout/AppLayout/NavBar.vue
@@ -1,8 +1,3 @@
-<script setup>
-import Menu from './menu.vue';
-// import { ref } from 'vue';
-
-</script>
 
 <template>
     <header id="navbar" class="grid grid-cols-5">
@@ -11,38 +6,44 @@ import Menu from './menu.vue';
                 <img class="ml-2 mt-1 sm:mt-2 sm:ml-6 scale-75" id="ptaLogo" src="/public/navbarIcons/pta-logo.B2o53BpU.png">
             </NuxtLink>
         </div>
-        <div id="naV" class="grid grid-cols-4 max-[1100px]:hidden col-span-3"> 
+        <div id="naV" class="grid grid-cols-4 max-[1024px]:hidden col-span-3"> 
              <div>
-                <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50 flex flex-row text-lg font-bold">
-                    <img class="size-8 mt-10 icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> 
+                <NuxtLink to="/" @click="('page=index')" class="w-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold">
+                    <img class="icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg"> 
                     <div class="self-end ml-2">HOME</div>
                 </NuxtLink>
              </div>
 
             <div>
-                <NuxtLink to="/events" class="size-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold">
+                <NuxtLink to="/events" class="w-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold">
                 <img class="icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg">
                 <div class="self-end ml-2">EVENTS</div>
             </NuxtLink>
             </div>
             
             <div>
-                <NuxtLink to="/contacts" class="size-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold whitespace-nowrap">
+                <NuxtLink to="/contacts" class="w-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold whitespace-nowrap">
                     <img class="icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg">
                     <div class="self-center ml-2">JOIN US</div>
                 </NuxtLink>
             </div>
 
             <div>
-                <NuxtLink to="/donate" class="size-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold">
+                <NuxtLink to="/donate" class="w-8 mt-10 hover:opacity-50 flex flex-row text-lg font-bold">
                     <img class="icon" id="donate" src="/public/navbarIcons/creditCard.svg">
                     <div class="self-end ml-2">DONATE</div>
                 </NuxtLink>
             </div>  
         </div>
-        <Menu class="visible lg:invisible"></Menu>
+        <Menu class="lg:hidden"></Menu>
     </header>
 </template>
+
+<script setup>
+import Menu from './menu.vue';
+// import { ref } from 'vue';
+
+</script>
 
 <style scoped>
 
--- pta/components/AppLayout/AppLayout/menu.vue
+++ pta/components/AppLayout/AppLayout/menu.vue
@@ -1,63 +1,81 @@
-<script setup>
-import gsap from 'gsap';
-import { ref } from 'vue';
-
-const show = ref(false)
-// const menuList = ref(null)
-
-function toggle(){
-    // show.value != show.value;
-
-    // console.log(show.value)
-
-    if (!show.value){
-        openMenu()
-    }
-}
-
-function openMenu(){
-    gsap.from(".menuList", {
-        x: 1000,
-    })
-}
-
-</script>
-
 <template>
     <div class="menu fixed top-0 left-0 right-0">
-        <div class="check fixed lg:top-10 lg:left-85 lg:right-5 top-10 lg-left-60 right-20">
-            <input v-model="show" @click="toggle" type="checkbox"/>
+        <div class="check fixed lg:top-10 lg:left-85 lg:right-5 top-10 lg-left-60 right-20 z-10">
+            <label for="check"></label>
+            <!-- <i class="fa-solid fa-bars"></i> -->
+            <input v-model="show" @click="toggle" type="checkbox" value="check"/>
         </div>
-
-      <div class="menuList">
-        <div v-if="show" class="justify-self-auto bg-gradient-to-r from-cyan-500 to-blue-500 w-screen h-dvh absolute">
-            <div class="mt-16 :mt-36 space-y-8">
-            <NuxtLink to="/" @click="('page=index')" class="hover:opacity-50 flex flex-row self-center text-lg font-bold">
-                <img class="size-12 icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg">
+    <Transition name="slide">
+      
+        <div v-if="show" class="justify-self-auto bg-gradient-to-b from-[#ffb393] via-[#fff1d7] to-[#fff] w-screen h-dvh absolute z-0">
+            <div class="mt-36 ml-16 space-y-8">
+            <NuxtLink to="/" @click="('page=index')" class="w-11 hover:opacity-50 flex flex-row self-center text-lg font-bold">
+                <img class="icon" id="home" src="/public/navbarIcons/house.DbJtZ1kr.svg">
                 <div class="self-center ml-2">HOME</div>
             </NuxtLink>
         
-            <NuxtLink to="/events" class="hover:opacity-50 flex flex-row self-center text-lg font-bold">
-                <img class="size-12 icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg">
+            <NuxtLink to="/events" class="w-11 hover:opacity-50 flex flex-row self-center text-lg font-bold">
+                <img class="icon" id="events" src="/public/navbarIcons/calendar.D_Rrp8xJ.svg">
                 <div class="self-center ml-2">EVENTS</div>
             </NuxtLink>
 
-            <NuxtLink to="/contacts" class="hover:opacity-50 flex flex-row self-center text-lg font-bold">
-                <img class="size-12 icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg">
+            <NuxtLink to="/contacts" class="w-11 hover:opacity-50 flex flex-row self-center text-lg font-bold">
+                <img class=" icon" id="contacts" src="/public/navbarIcons/envelope.BpFCkuaU.svg">
                 <div class="self-center ml-2 whitespace-nowrap">JOIN US</div>
             </NuxtLink>
 
-            <NuxtLink to="/donate" class="hover:opacity-50 flex flex-row self-center text-lg font-bold">
-                <img class="size-12 icon" id="donate" src="/public/navbarIcons/creditCard.svg">
+            <NuxtLink to="/donate" class="w-11 hover:opacity-50 flex flex-row self-center text-lg font-bold">
+                <img class="icon" id="donate" src="/public/navbarIcons/creditCard.svg">
                 <div class="self-center ml-2">DONATE</div>
             </NuxtLink>
             </div>
         </div>
-    </div>
+    
+    </Transition>
   </div>
 </template>
 
+<script setup>
+
+</script>
+
 
 <style scoped>
+@keyframes slide-right {
+  0% {
+    right: -100%;
+  }
+  50% {
+    right: -100%;
+  }
+  100% {
+    right: 0;
+  }
+}
 
+@keyframes slide-left {
+  0% {
+    right: 0%;
+  }
+  100% {
+    right: -100%;
+  }
+}
+
+/* .slide-enter-active {
+  animation: slide-right 0.4s;
+}
+
+.slide-leave-from,
+.slide-leave-active {
+  animation: slide-left 0.4s
+} */
+
+.slide-enter-active, .slide-leave-active {
+  animation: slide-right 0.6s;
+}
+
+.slide-enter-from, .slide-leave-to {
+  animation: slide-left 0.3s
+}
 </style>
