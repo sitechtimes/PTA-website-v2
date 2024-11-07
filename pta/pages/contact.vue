9ca8cbf74497446bae07f0108235ee88ab3f5158
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-11-06 23:34:31 -0500

    a

--- pta/pages/contact.vue
+++ pta/pages/contact.vue
@@ -4,22 +4,22 @@
         <div id="buttons" class="text-3xl mt-8 font-semibold ">
             <button id="join" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'join'}" 
-                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-1/5 py-2 rounded-xl xl:mx-6">Join Us</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-2/12 py-2 rounded-xl xl:mx-6">Join Us</button>
             <button id="staff" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'staff'}" 
-                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-1/5 py-2 rounded-xl xl:mx-6">Staff</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-2/12 py-2 rounded-xl xl:mx-6">Staff</button>
             <button id="slt" @click="buttonClick($event)"
                 :class="{'bg-white': buttonType === 'slt'}" 
-                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-1/5 py-2 rounded-xl xl:mx-6">SLT</button>
+                class="mx-1 hover:opacity-50 text-lg md:text-2xl xl:text-3xl w-3/12 md:w-2/12 xl:w-2/12 py-2 rounded-xl xl:mx-6">SLT</button>
         </div>
         <div class="flex justify-center my-6">
             <div v-if="buttonType === 'join'" class="bg-white rounded-3xl w-10/12 md:w-9/12 xl:w-7/12 p-10">
                 <p>Join us</p>
             </div>
-            <div v-if="buttonType === 'staff'" class="bg-white rounded-3xl w-10/12 p-10">
+            <div v-if="buttonType === 'staff'" class="bg-white rounded-3xl w-10/12 md:w-9/12 xl:w-7/12 p-10">
                 <p>Different staff</p>
             </div>
-            <div v-if="buttonType === 'slt'" class="bg-white rounded-3xl w-10/12 p-10">
+            <div v-if="buttonType === 'slt'" class="bg-white rounded-3xl w-10/12  md:w-9/12 xl:w-7/12 p-10">
                 <p>The SLT</p>
             </div>
         </div>
@@ -31,6 +31,11 @@ const buttonType = ref('join');
 function buttonClick(event) {
     buttonType.value = event.target.id;
 }
+gsap.from(".container", {
+  opacity: 0, 
+  y: 100, 
+  duration: 1
+});
 </script>
 
 <style scoped></style>
