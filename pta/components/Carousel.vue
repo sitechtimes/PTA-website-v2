c049ea0ff2b599e6c8753c164ec3536c232aea4f
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-11-14 00:22:58 -0500

    A

--- pta/components/Carousel.vue
+++ pta/components/Carousel.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="carousel w-1/3 lg:float-right flex">
-  <div id="slide1" class="carousel-item relative w-full">
+    <div class="carousel w-full lg:float-right flex">
+  <div id="slide1" class="carousel-item relative w-full h-1/2">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+      src="/public/photos/carousel1.jpg"
       class="w-full" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide4" class="btn btn-circle">❮</a>
@@ -11,7 +11,7 @@
   </div>
   <div id="slide2" class="carousel-item relative w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+      src="/public/photos/carousel2.png"
       class="w-full" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" class="btn btn-circle">❮</a>
@@ -20,7 +20,7 @@
   </div>
   <div id="slide3" class="carousel-item relative w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+      src="/public/photos/carousel3.jpg"
       class="w-full" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" class="btn btn-circle">❮</a>
@@ -29,10 +29,64 @@
   </div>
   <div id="slide4" class="carousel-item relative w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      src="/public/photos/carousel4.jpg"
       class="w-full" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" class="btn btn-circle">❮</a>
+      <a href="#slide5" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide5" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel5.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" class="btn btn-circle">❮</a>
+      <a href="#slide6" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide6" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel6.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide5" class="btn btn-circle">❮</a>
+      <a href="#slide7" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide7" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel7.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide6" class="btn btn-circle">❮</a>
+      <a href="#slide8" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide8" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel8.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide7" class="btn btn-circle">❮</a>
+      <a href="#slide9" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide9" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel9.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide8" class="btn btn-circle">❮</a>
+      <a href="#slide10" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide10" class="carousel-item relative w-full">
+    <img
+      src="/public/photos/carousel10.jpg"
+      class="w-full" />
+    <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide9" class="btn btn-circle">❮</a>
       <a href="#slide1" class="btn btn-circle">❯</a>
     </div>
   </div>
--- pta/components/MiddleSection.vue
+++ pta/components/MiddleSection.vue
@@ -1,27 +1,23 @@
 <template>
-    <div id="aboutUs">
+  <div id="aboutUs">
     <h2 class="text-xl md:text-2xl lg:text-4xl font-bold my-6">ABOUT US</h2>
     <div class="text-base md:text-lg lg:text-xl mt-5 lg:my-10">
-    <p>The mission of Staten Island Tech’s PTA is to support our school through advocacy, volunteerism and fundraising.
-        From our bake sales to our annual Spring Gala, we work diligently to raise money in order to financially support
-        our school’s needs such as online and software services, facility upgrades and social events / celebrations.</p>
-    <p class="mt-3">While Staten Island Tech provides a strong academic environment for our children, the school cannot do this
-        alone. Participating with the PTA is vital to our students and school community. Please join our meeting each
-        month to learn about the ongoing activities and events that help make our school a success. Also, <b>please support
-        our school with our suggested donations towards PTA dues. No amount is too small or too great.</b></p>
+      <p>The mission of Staten Island Tech’s PTA is to support our school through advocacy, volunteerism, and fundraising.
+          From our bake sales to our annual Spring Gala, we work diligently to raise money in order to financially support
+          our school’s needs such as online and software services, facility upgrades, and social events / celebrations.</p>
+      <p class="mt-3">While Staten Island Tech provides a strong academic environment for our children, the school cannot do this
+          alone. Participating with the PTA is vital to our students and school community. Please join our meeting each
+          month to learn about the ongoing activities and events that help make our school a success. Also, <b>please support
+          our school with our suggested donations towards PTA dues. No amount is too small or too great.</b></p>
     </div>
-    </div>
-    <div id="activities">
+  </div>
+  <div id="activities">
     <h2 class="text-xl md:text-2xl lg:text-4xl font-bold my-6">ACTIVITIES</h2>
-    <p class="text-base md:text-lg lg:text-xl mt-5 lg:my-10">The SITHS PTA is a group of parents and faculty/staff members who work
-       together to provide our children with resources and activities to make
-       their school experience the best ever!</p>
-       <Carousel/>
+    <div class="flex flex-wrap lg:flex-nowrap lg:items-start lg:space-x-6">
+      <p class="text-base md:text-lg lg:text-xl mt-5 lg:my-10 lg:w-2/3">
+        The SITHS PTA is a group of parents and faculty/staff members who work together to provide our children with resources and activities to make their school experience the best ever!
+      </p>
+      <Carousel class="w-full lg:w-1/3"/>
+    </div>
   </div>
 </template>
-
-<script setup>
-
-</script>
-
-<style scoped></style>
