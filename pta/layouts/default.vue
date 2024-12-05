59b8cd9d9bbc599572e33e9175b8ff3e0c23b16e
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-05 10:39:11 -0500

    somethng

--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
       <slot />
+      <Resources/>
       <AppLayoutFooter />
     </div>
   </template>
   
   <script setup>
+import Resources from '~/components/AppLayout/resources.vue';
+
 </script>
   
   <style scoped></style>
