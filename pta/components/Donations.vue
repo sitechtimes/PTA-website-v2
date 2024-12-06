2a3e8162a2909124ec0057e866324943c65c9a28
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-12-06 10:59:43 -0500

    a

--- pta/components/Donations.vue
+++ pta/components/Donations.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="mx-24">
-    <h1 class="text-5xl font-bold ">DONATIONS</h1>
+    <h1 class="text-5xl font-bold">DONATIONS</h1>
     <div class="my-12">
         <h2 class="text-3xl font-bold">Bake Sales</h2>
         <p class="text-2xl my-4">Food and drink donations would be much appreciated to help launch bake sales during the year. Our students always enjoy these added treats to their daily lunch routine.</p>
         <p class="text-2xl my-4">Here is a wish list of items that we would like to provide to our students which include : Cookies, Brownies, Plates, Chips, Beverages, Cup Noodles, and any other baked goods. <b>(PLEASE NO NUTS)</b></p>
         <p class="text-2xl my-4">You can sign up for items you wish to bring in on the google form!</p>
-        <button class="p-4 rounded-lg my-2">Donate Item</button>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfoRbBX2iCx20Kxcud0IcjT2UhmpjaNxDda78V3BIfN_Mw3Dg/viewform">
+            <button class="p-4 font-bold rounded-lg my-2 bg-dark-brown text-white">Donate Item</button>
+        </a>
     </div>
-    <div class="my-12">
+    <div class="my-16">
         <h2 class="text-3xl font-bold">Monetary Donations</h2>
         <p class="text-2xl my-4">Monetary donations are always graciously accepted throughout the school year. If you have already donated, we thank you for your generosity and support.</p>
         <p class="text-2xl my-4">To donate, please bring the completed form and donation to Ms. Barbara Malenfant’s office, Room 117</p>
