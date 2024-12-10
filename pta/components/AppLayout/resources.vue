257876d0edcf55ea2d738d049765221c5d85dccd
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-09 20:54:11 -0500

    rows -> cols

--- pta/components/AppLayout/resources.vue
+++ pta/components/AppLayout/resources.vue
@@ -1,30 +1,26 @@
 <template>
     <header>RESOURCES</header>
     <div class="grid lg:grid-rows-2 lg:grid-cols-2 grid-flow-col gap-4 grid-rows-4 ">
-        <div class="bg-black rounded-xl grid grid-rows-3 grid-flow-col gap-4">
+        <div class="bg-black rounded-xl grid grid-cols-3 grid-flow-col gap-4">
             <img src="/resources/NEWSITHS_Logo.png" class="w-44 place-content-center row-span-3">
             <div class="col-span-2">SITHS Automated Telephone Message Directory</div>
             <div class="col-span-2">Contact Staten Island Technical High School</div>
             <NuxtLink to="https://www.siths.org/apps/pages/index.jsp?uREC_ID=1555040&type=d&pREC_ID=1680913" class="bg-white rounded-full">READ MORE</NuxtLink>
         </div>
-
-        <div class="bg-black rounded-xl grid grid-rows-3 grid-flow-col gap-4">
+        <div class="bg-black rounded-xl grid grid-cols-3 grid-flow-col gap-4 ">
             <img src="/resources/Naviance-Logo.png" class="w-44 place-content-center row-span-3">
             <div class="col-span-2">Naviance</div>
             <div class="col-span-2">Track Secondary Goals and Success</div>
             <NuxtLink to="https://id.naviance.com/" class="bg-white rounded-full">READ MORE</NuxtLink>
-            
         </div>
-
-        <div class="bg-black rounded-xl grid grid-rows-3 grid-flow-col gap-4">
-            <img src="/resources/NycDoeLogo.png" class="w-60 place-content-center row-span-3">
+        <div class="bg-black rounded-xl grid grid-cols-3 grid-flow-col gap-4">
+            <img src="/resources/NycDoeLogo.png" class="w-52 place-content-center row-span-3">
             <div class="col-span-2">NYCDOE Websites</div>
             <div class="col-span-2">Keep Track of Student Documents and Official City Calendars</div>
             <NuxtLink to="https://www.schools.nyc.gov/"  class="bg-white rounded-full">READ MORE</NuxtLink>
         </div>
-            
-        <div class="bg-black rounded-xl grid grid-rows-3 grid-flow-col gap-4">
-            <img src="/logo/pta-logo.png" class="w-56 place-content-center row-span-3">
+        <div class="bg-black rounded-xl grid grid-cols-3 grid-flow-col gap-4 ">
+            <img src="/logo/pta-logo.png" class="w-52 place-content-center row-span-3">
             <div class="col-span-2">SITHS PTA By-Laws</div>
             <div class="col-span-2">Read More About the PTA</div>
             <NuxtLink to="https://docs.google.com/document/d/1gNyKPMDOUxeW2AadJFuOXtla3jd_bzjw/edit?usp=sharing&ouid=109431569121511580485&rtpof=true&sd=true"
