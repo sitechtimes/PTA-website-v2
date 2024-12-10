b86626b95cfb4f07b3a4fb3f01551a473a8e216c
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-10 13:32:22 -0500

    i think im done
    just a note that for some reason the font doesn;t work

--- pta/components/AppLayout/resources.vue
+++ pta/components/AppLayout/resources.vue
@@ -1,43 +1,47 @@
 <template>
-    <header>RESOURCES</header>
-    <div class="grid lg:grid-rows-2 lg:grid-cols-2 grid-flow-col gap-4 grid-rows-4">
-        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4">
+    <header class="text-4xl font-bold m-5 font-kumbha">RESOURCES</header>
+    <div class="grid lg:grid-rows-2 lg:grid-cols-2 grid-flow-col gap-4 grid-rows-4 ml-5 mr-5">
+        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 h-64
+                    shadow-lg border border-primary border-opacity-60 border-[#312217]">
             <img src="/resources/NEWSITHS_Logo.png" class="p-4 w-44 place-content-center row-span-3 justify-center self-center">
-            <div class="col-span-2">SITHS Automated Telephone Message Directory</div>
-            <div class="col-span-2">Contact Staten Island Technical High School</div>
+            <div class="col-span-2 font-bold text-2xl place-content-end">Automated Telephone Message Directory</div>
+            <div class="col-span-2 text-lg">Contact Staten Island Technical High School.</div>
             <NuxtLink to="https://www.siths.org/apps/pages/index.jsp?uREC_ID=1555040&type=d&pREC_ID=1680913" 
             class="bg-[#C9B23A] rounded-full text-center place-content-center font-semibold text-white
-            transition duration-150 ease-out hover:ease-in hover:opacity-50">
+            transition duration-150 ease-out hover:ease-in hover:opacity-50 mb-8">
                 Read More
             </NuxtLink>
         </div>
-        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 ">
+        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 h-64 
+                    shadow-lg border border-primary border-opacity-60 border-[#312217]">
             <img src="/resources/Naviance-Logo.png" class="p-4 w-44 place-content-center row-span-3 self-center">
-            <div class="col-span-2">Naviance</div>
-            <div class="col-span-2">Track Secondary Goals and Success</div>
+            <div class="col-span-2 font-bold text-2xl place-content-end">Naviance</div>
+            <div class="col-span-2 text-lg">Track Secondary Goals and Success</div>
             <NuxtLink to="https://id.naviance.com/" 
             class="bg-[#C9B23A] rounded-full text-center place-content-center font-semibold text-white
-            transition duration-150 ease-out hover:ease-in hover:opacity-50">
+            transition duration-150 ease-out hover:ease-in hover:opacity-50 mb-8">
                 Read More
             </NuxtLink>
         </div>
-        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4">
+        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 h-64
+                    shadow-lg border border-primary border-opacity-60 border-[#312217]">
             <img src="/resources/NycDoeLogo.png" class="p-4 w-52 place-content-center row-span-3 self-center">
-            <div class="col-span-2">NYCDOE Websites</div>
-            <div class="col-span-2">Keep Track of Student Documents and Official City Calendars</div>
+            <div class="col-span-2 font-bold text-2xl place-content-end">NYCDOE Websites</div>
+            <div class="col-span-2 text-lg">Keep Track of Student Documents and Official City Calendars</div>
             <NuxtLink to="https://www.schools.nyc.gov/"  
             class="bg-[#C9B23A] rounded-full text-center place-content-center font-semibold text-white
-            transition duration-150 ease-out hover:ease-in hover:opacity-50">
+            transition duration-150 ease-out hover:ease-in hover:opacity-50 mb-8">
                 Read More
             </NuxtLink>
         </div>
-        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 ">
+        <div class="bg-white rounded-xl grid grid-cols-3 grid-flow-col gap-4 h-64
+                    shadow-lg border border-primary border-opacity-60 border-[#312217]">
             <img src="/logo/pta-logo.png" class="p-4 w-52 place-content-center row-span-3 self-center">
-            <div class="col-span-2">SITHS PTA By-Laws</div>
-            <div class="col-span-2">Read More About the PTA</div>
+            <div class="col-span-2 font-bold text-2xl place-content-end">SITHS PTA By-Laws</div>
+            <div class="col-span-2 text-lg">Read More About the PTA</div>
             <NuxtLink to="https://docs.google.com/document/d/1gNyKPMDOUxeW2AadJFuOXtla3jd_bzjw/edit?usp=sharing&ouid=109431569121511580485&rtpof=true&sd=true"
-            class="bg-[#C9B23A] rounded-full text-center place-content-center font-semibold text-white 
-            transition duration-150 ease-out hover:ease-in hover:opacity-50">
+            class="bg-[#C9B23A] rounded-full text-center place-content-center font-semibold text-white
+            transition duration-150 ease-out hover:ease-in hover:opacity-50 mb-8">
                 Read More
                 <!-- btw the doc links to an unseeable doc  -->
             </NuxtLink>
