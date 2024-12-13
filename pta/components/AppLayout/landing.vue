48da8dbf703cc2540c2e8d125ae49e5880d1ccce
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-13 11:00:58 -0500

    basic stuff

---
+++ pta/components/AppLayout/landing.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="grid grid-rows-2 lg:grid-cols-2">
+        <div class="m-16 mt-6">
+            <div class="font-normal">Parent Teacher Association @</div>
+            <div class="font-extrabold text-7xl">Staten Island Technical High School</div>
+            <div class="font-normal">485 Clawson Street, Staten Island, NY 10306</div>
+        </div>
+        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 m-16 mt-6">events</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
+      <Landing/>
       <slot />
       <AppLayoutFooter />
     </div>
   </template>
   
   <script setup>
+import Landing from '~/components/AppLayout/landing.vue';
+
 </script>
   
   <style scoped></style>
