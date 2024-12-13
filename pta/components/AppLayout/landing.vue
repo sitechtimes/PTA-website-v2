a08f27623fdf9dec5a4694630b96fc8d7f5b84e3
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-13 13:04:37 -0500

    👍

--- pta/components/AppLayout/landing.vue
+++ pta/components/AppLayout/landing.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="grid grid-rows-2 lg:grid-cols-2">
-        <div class="m-16 mt-6">
+    <div class="grid grid-rows-2 lg:grid-cols-2 m-4 mt-12">
+        <div class="mx-4 md:mx-16">
             <div class="font-normal">Parent Teacher Association @</div>
-            <div class="font-extrabold text-7xl">Staten Island Technical High School</div>
-            <div class="font-normal">485 Clawson Street, Staten Island, NY 10306</div>
+            <div class="font-extrabold text-6xl md:text-7xl xl:text-8xl">Staten Island Technical High School</div>
+            <div class="font-normal mt-4">485 Clawson Street, Staten Island, NY 10306</div>
         </div>
-        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 m-16 mt-6">events</div>
+        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 mx-4 md:mx-16 lg:mx-10">events</div>
     </div>
 </template>
 
