1c7d63df41e00708b5f3244291dd205fe94d34d8
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-12-16 11:01:22 -0500

    joinus

---
+++ pta/components/JoinUs.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2 class="font-bold text-2xl">PARENT VOLUNTEER OPPORTUNITIES</h2>
+        <p class="text-xl">If you would like to volunteer at PTA events, please print out the attached volunteer form and submit to the
+            PTA Room or email it to sitechpta@gmail.com.</p>
+        <p>Be part of the SI TECH PTA Grant Committee</p>
+        <p>The grants committee researches and applies for grants to help fund school programs and activities including
+            classroom/school upgrades, art, technology, maker-space, and the library. We reach out to foundations and
+            corporations.</p>
+        <p>Volunteers are needed to research grant opportunities or help write the grant proposals. No prior experience
+            is necessary, time commitment is contingent on grant deadlines.</p>
+        <p>Please email SITHSptagrantcommittee@gmail.com if interested.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
