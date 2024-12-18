e04c44499d827d6847893c973f7e4926b58e35e5
katherine <116115224+kaerulean@users.noreply.github.com>
2024-12-18 10:45:08 -0500

    yeah

--- pta/pages/Events.vue
+++ pta/pages/Events.vue
@@ -1,7 +1,19 @@
 <template>
+    <div class="grid grid-rows-2 md:grid-cols-2 gap-4 m-8 mt-16">
+        <div class="grid grid-rows-3 gap-y-6">
+            <div class="font-bold text-5xl">EVENTS</div>
+            <div class="font-bold text-2xl">UPCOMING EVENTS</div>
+            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+        </div>
+
+        <div class="bg-gradient-to-r from-cyan-500 to-blue-500">
+            calendar
+        </div>
+    </div>
 </template>
 
 <script setup>
+
 </script>
 
 <style scoped>
