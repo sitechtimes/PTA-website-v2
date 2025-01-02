700b38453dc286eb77bab456958665acfed187c3
bridgetf7 <fbridget0404@gmail.com>
2025-01-01 22:14:16 -0500

    fixed file system + route name for /contacts

--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-      <NavBar/>
-      <slot />
-      <AppLayoutFooter />
-    </div>
-  </template>
-  
-  <script setup>
+  <div>
+    <AppLayoutNavBar class="sticky top-0 z-50" />
+    <slot />
+    <AppLayoutFooter />
+  </div>
+</template>
+
+<script setup>
 </script>
-  
-  <style scoped></style>
+
+<style scoped></style>
