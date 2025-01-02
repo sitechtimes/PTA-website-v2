19abf5c6247a38126258685c7add0c0b7e0cd17b
katherine <116115224+kaerulean@users.noreply.github.com>
2025-01-02 10:32:11 -0500

    pee pee

--- pta/pages/Events.vue
+++ pta/pages/Events.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="grid grid-rows-2 md:grid-cols-2 gap-4 m-8 mt-16">
-        <div class="grid grid-rows-3 gap-y-6">
-            <div class="font-bold text-5xl">EVENTS</div>
-            <div class="font-bold text-2xl">UPCOMING EVENTS</div>
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+        <div class="grid grid-rows-3 gap-y-3">
+            <div class="font-bold text-5xl md:text-6xl">EVENTS</div>
+            <div class="font-bold text-2xl md:text-3xl">UPCOMING EVENTS</div>
+            <div class="">
+                <ul class="flex flex-col gap-y-1">
+                    <li class="bg-white">hsdjadsha</li>
+                    <li class="bg-white">hsdjadsha</li>
+                    <li class="bg-white">hsdjadsha</li>
+                </ul>
+            </div>
         </div>
 
         <div class="bg-gradient-to-r from-cyan-500 to-blue-500">
