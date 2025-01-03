eba2da747c5ec80ff95ca70f8dcad3700d2873d1
katherine <116115224+kaerulean@users.noreply.github.com>
2025-01-03 10:58:03 -0500

    nhhhcbbnjyrte

--- pta/pages/Events.vue
+++ pta/pages/Events.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="grid grid-rows-2 md:grid-cols-2 gap-4 m-8 mt-16">
-        <div class="grid grid-rows-3 gap-y-3">
-            <div class="font-bold text-5xl md:text-6xl">EVENTS</div>
-            <div class="font-bold text-2xl md:text-3xl">UPCOMING EVENTS</div>
+        <div class="grid grid-rows-3">
+            <div class="font-extrabold text-7xl self-center">EVENTS</div>
+            <div class="font-extrabold text-3xl self-center">UPCOMING EVENTS</div>
             <div class="">
-                <ul class="flex flex-col gap-y-1">
-                    <li class="bg-white">hsdjadsha</li>
-                    <li class="bg-white">hsdjadsha</li>
-                    <li class="bg-white">hsdjadsha</li>
+                <ul class="flex flex-col gap-y-2">
+                    <li class="bg-white shadow-lg">hsdjadsha</li>
+                    <li class="bg-white shadow-lg">hsdjadsha</li>
+                    <li class="bg-white shadow-lg">hsdjadsha</li>
                 </ul>
             </div>
         </div>
