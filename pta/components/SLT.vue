973edf2c55f7f437f2273fa8f45799376c1c9bbe
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-01-07 10:22:51 -0500

    a

--- pta/components/SLT.vue
+++ pta/components/SLT.vue
@@ -2,10 +2,10 @@
     <div class="flex flex-wrap">
     <div v-for="(profile, index) in profiles"
     :key="index"
-     id="profile" class="content-center lg:w-1/2 2xl:w-1/3 my-4 flex items-center flex-col">
-        <img src="/pfps/default-profile.webp" alt="pfp" class="w-1/4 rounded-full">
-        <h2 class="text-base md:text-xl my-4 font-bold">{{ profile.name }}</h2>
-        <p class="text-base md:text-xl mt-2 mb-6">{{ profile.email }}</p>
+     id="profile" class="content-center md:w-1/2 2xl:w-1/3 my-4 flex items-center flex-col">
+        <img src="/pfps/default-profile.webp" alt="pfp" class="w-5/12 rounded-full">
+        <h2 class="text-base sm:text-xl my-4 font-bold">{{ profile.name }}</h2>
+        <p class="text-sm sm:text-lg mt-2 mb-6">{{ profile.email }}</p>
     </div>
 </div>
 </template>
--- pta/components/Staff.vue
+++ pta/components/Staff.vue
@@ -2,11 +2,12 @@
     <div class="flex flex-wrap">
     <div v-for="(profile, index) in profiles"
     :key="index"
-     id="profile" class="content-center lg:w-1/2 2xl:w-1/3 my-4 flex items-center flex-col">
-        <img :src="profile.pfp" alt="pfp" class="w-1/2 rounded-full">
-        <h2 class="text-base md:text-xl my-4 font-bold">{{ profile.name }}</h2>
-        <p class="text-base md:text-xl mt-2 mb-6">{{ profile.position }}</p>
-        <p class="text-sm md:text-lg mt-2 mb-6">{{ profile.email }}</p>
+     id="profile" class="content-center md:w-1/2 2xl:w-1/3 my-4 flex items-center flex-col">
+        <img :src="profile.pfp" alt="pfp" class="rounded-full w-5/12">
+        <h2 class="text-base sm:text-xl my-4 font-bold">{{ profile.name }}</h2>
+        <p class="text-base sm:text-xl mt-2 mb-6">{{ profile.position }}</p>
+        <p class="text-sm sm:text-lg mt-2">{{ profile.email }}</p>
+        <p class="text-sm sm:text-lg  mt-4 mb-6">{{ profile.phone }}</p>
     </div>
 </div>
 </template>
@@ -24,24 +25,28 @@ const profiles = [
         pfp: "/pfps/default-profile.webp",
         email: "nilar.n.tun@protonmail.com",
         position: "Co-Vice President",
+        phone: "347-997-2381",
     },
     {
         name: "Yelena Mike",
         pfp: "/pfps/default-profile.webp",
         email: "yelena.mike@yahoo.com",
         position: "Co-Vice President",
+        phone: "646-637-7711",
     },
     {
         name: "Miriam Nagornuk",
         pfp: "/pfps/default-profile.webp",
         email: "mimikngk@gmail.com",
         position: "Co-Vice President",
+        phone: "347-596-5942",
     },
     {
         name: "Laura Sulem",
         pfp: "/pfps/default-profile.webp",
         email: "laurasulem@gmail.com",
         position: "Corresponding Secretary",
+        phone: "646-322-6311",
     },
     {
         name: "Lorena Brown",
@@ -54,36 +59,42 @@ const profiles = [
         pfp: "/pfps/NycDoeLogo.png",
         email: "sarah.a.celebi@gmail.com",
         position: "Freshmen Representative",
+        phone: "917-741-8797",
     },
     {
         name: "Barbara Malenfant",
         pfp: "/pfps/NycDoeLogo.png",
         email: "BMalenfant@schools.nyc.gov",
         position: "Parent Coordinator",
+        phone: "347-563-4563",
     },
     {
         name: "Sylvia Pace-Diaz",
         pfp: "/pfps/default-profile.webp",
         email: "spacediaz@gmail.com",
         position: "Parliamentarian",
+        phone: "917-854-7804",
     },
     {
         name: "Dennis Owens",
         pfp: "/pfps/buro.jpg",
         email: "pooh@aloha.net",
         position: "President",
+        phone: "718-448-8408",
     },
     {
         name: "Jean-Marie Bruzzese",
         pfp: "/pfps/default-profile.webp",
         email: "jeanmb@gmail.com",
         position: "Recording Secretary",
+        phone: "917-376-3048",
     },
     {
         name: "Julie Baskevich",
         pfp: "/pfps/default_pfp.svg.png",
         email: "JulieBaskevich@gmail.com",
         position: "Treasurer",
+        phone: "718-938-9762",
     },
 ]
 </script>
