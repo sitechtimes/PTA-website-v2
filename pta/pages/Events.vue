466a7f45b3e31e28d236b4b6fa828feb83576eb4
katherine <116115224+kaerulean@users.noreply.github.com>
2025-01-07 15:08:02 -0500

    erm

--- pta/pages/Events.vue
+++ pta/pages/Events.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="grid grid-rows-2 md:grid-cols-2 gap-4 m-8 mt-16">
-        <div class="grid grid-rows-3">
-            <div class="font-extrabold text-7xl self-center">EVENTS</div>
-            <div class="font-extrabold text-3xl self-center">UPCOMING EVENTS</div>
+<div class="static">
+    <div class="grid grid-rows-2 md:grid-cols-2 gap-12 m-8 mt-16">
+        <div class="rows-2">
             <div class="">
-                <ul class="flex flex-col gap-y-2">
-                    <li class="bg-white shadow-lg">hsdjadsha</li>
-                    <li class="bg-white shadow-lg">hsdjadsha</li>
-                    <li class="bg-white shadow-lg">hsdjadsha</li>
-                </ul>
+                <div class="font-extrabold text-7xl self-center">EVENTS</div>
+                <div class="font-extrabold text-3xl self-center my-6">UPCOMING EVENTS</div>
+            </div>
+        
+            <div class="overflow-auto max-h-60">
+                <div class="flex flex-col gap-y-8">
+                    <div class="bg-white shadow-lg md:w-4/5" @click="wow">hsdjadsha</div>
+                    <div class="bg-white shadow-lg md:w-4/5">hsdjadsha</div>
+                    <div class="bg-white shadow-lg md:w-4/5">hsdjadsha</div>
+                    <div class="bg-white shadow-lg md:w-4/5">hsdjadsha</div>
+                    <div class="bg-white shadow-lg md:w-4/5">hsdjadsha</div>
+                </div>
             </div>
         </div>
-
         <div class="bg-gradient-to-r from-cyan-500 to-blue-500">
             calendar
         </div>
     </div>
+
+    <div v-if="appear" class="fixed top-0 left-0">
+        <div class="bg-white shadow-lg">
+            box
+        </div>
+    </div>
+</div>
 </template>
 
 <script setup>
+const appear = ref(true)
+
+function wow(){
+    appear.value = true
+}
+
 
 </script>
 
