fc98528aa4aa880426f4f36a71615fb47c07cea4
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-01-10 11:00:35 -0500

    changes made

--- pta/components/MiddleSection.vue
+++ pta/components/MiddleSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="aboutUs">
-    <h2 class="text-xl md:text-2xl lg:text-4xl font-bold mb-6">ABOUT US</h2>
-    <div class="text-base md:text-lg lg:text-2xl mt-5 lg:my-10">
+    <h2 class="text-xl md:text-2xl lg:text-4xl font-bold">ABOUT US</h2>
+    <div class="text-base md:text-lg lg:text-2xl mt-2 lg:my-6">
       <p>The mission of Staten Island Tech’s PTA is to support our school through advocacy, volunteerism, and fundraising.
           From our bake sales to our annual Spring Gala, we work diligently to raise money in order to financially support
           our school’s needs such as online and software services, facility upgrades, and social events / celebrations.</p>
@@ -12,9 +12,9 @@
     </div>
   </div>
   <div id="activities">
-    <h2 class="text-xl md:text-2xl lg:text-4xl font-bold my-6">ACTIVITIES</h2>
+    <h2 class="text-xl md:text-2xl lg:text-4xl font-bold mt-6">ACTIVITIES</h2>
     <div class="flex flex-wrap lg:flex-nowrap lg:items-start lg:space-x-6">
-      <p class="text-base md:text-lg lg:text-2xl mt-5 lg:my-10 lg:w-2/3">
+      <p class="text-base md:text-lg lg:text-2xl mt-2 lg:my-6 lg:w-2/3">
         The SITHS PTA is a group of parents and faculty/staff members who work together to provide our children with resources and activities to make their school experience the best ever!
       </p>
       <Carousel class="w-full lg:w-1/3"/>
