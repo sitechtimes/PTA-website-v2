c0112c49b14d22f26da05f652af50bfd111f2a56
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-01-13 10:38:34 -0500

    text wrap

--- pta/components/SLT.vue
+++ pta/components/SLT.vue
@@ -4,8 +4,8 @@
     :key="index"
      id="profile" class="content-center w-1/2 2xl:w-1/3 my-4 flex items-center text-center flex-col">
         <img src="/pfps/default-profile.webp" alt="pfp" class="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36">
-        <h2 class="text-base sm:text-xl mt-2 font-bold">{{ profile.name }}</h2>
-        <p class="text-sm sm:text-lg mt-2 mb-4">{{ profile.email }}</p>
+        <h2 class="text-sm sm:text-xl mt-2 font-bold text-nowrap">{{ profile.name }}</h2>
+        <p class="text-xs sm:text-lg mt-2 text-wrap">{{ profile.email }}</p>
     </div>
 </div>
 </template>
@@ -26,7 +26,7 @@ const profiles = [
     },
     {
         name: "Michael Shmulsenson",
-        email: "MichaelShmulsenson@gmail.com",
+        email: "MichaelShmulsenson @gmail.com",
     },
     {
         name: "Mike Nguyen",
