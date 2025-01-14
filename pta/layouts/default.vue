0551e340a4ffb44fc5a397b1665326110d463072
katherine <116115224+kaerulean@users.noreply.github.com>
2025-01-14 10:33:52 -0500

    asdnn

--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-      <Landing/>
       <slot />
       <AppLayoutFooter />
     </div>
   </template>
   
   <script setup>
-import Landing from '~/components/AppLayout/landing.vue';
+
 
 </script>
   
--- pta/pages/index.vue
+++ pta/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
+
     <!-- <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1> -->
+    <AppLayoutLanding/>
 </template>
 
 <script setup>
