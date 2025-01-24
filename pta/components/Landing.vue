26b78e4d12bf7b00f46b6d31d25251980e8a3b5f
bridgetf7 <fbridget0404@gmail.com>
2025-01-24 18:02:52 -0500

    landing section font adjustments

--- pta/components/Landing.vue
+++ pta/components/Landing.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="grid grid-rows-2 lg:grid-cols-2 my-4 mt-12 mx-2">
         <div class="mx-4 md:mx-16">
-            <div class="font-normal opacity-0" id="PTA">Parent Teacher Association @</div>
-            <div class="font-bold text-6xl md:text-7xl xl:text-8xl" id="SITHS">STATEN ISLAND TECHNICAL HIGH SCHOOL</div>
-            <div class="font-normal my-4" id="address">485 Clawson Street, Staten Island, NY 10306</div>
+            <h2 class="font-normal lg:text-2xl md:xl" id="PTA">Parent Teacher Association @</h2>
+            <h1 class="font-bold tracking-tight text-6xl md:text-7xl xl:text-8xl" id="SITHS">STATEN ISLAND TECHNICAL HIGH SCHOOL</h1>
+            <p class="font-normal my-4 lg:text-2xl md:xl" id="address">485 Clawson Street, Staten Island, NY 10306</p>
         </div>
-        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 mx-4 md:mx-16 lg:mx-10">events</div>
+        <p class="bg-gradient-to-r from-cyan-500 to-blue-500 mx-4 md:mx-16 lg:mx-10">events</p>
     </div>
 </template>
 
